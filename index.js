170f9e2815844017dd7345574e738371be3bb2d8
ukrwebprom <ukrwebprom@gmail.com>
2023-04-17 17:03:51 +0300

    users

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.on('connection', (ws) => {
                 ws,
                 user
             })
-            console.log("new user, added")
+            console.log("new user, added", users)
             }
     
         sendToAll(user.ID, user);
