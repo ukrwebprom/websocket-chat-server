5bba6c27cf32f337efe1878bad562273a6c88969
ukrwebprom <ukrwebprom@gmail.com>
2023-04-18 09:57:39 +0300

    killTimeout

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var sr = require('simple-random');
 const PORT = process.env.PORT || 8080;
 const server = new WebSocket.Server({ port:PORT });
 const users = [];
+let killTimeout = {};
 console.log("Welcome to websocket chat server", PORT);
 
 const noSuchUser = (uid, chatID) => {
@@ -13,17 +14,27 @@ const sendToAll = (id, message) => {
     users.filter(u => u.chatID === id).forEach(e => {e.ws.send(JSON.stringify(message))});
 }
 
+const removeUser = (currentUser) => {
+    users.map(u => u.uid !== currentUser.uid && u.chatID !== currentUser.ID);
+    clearTimeout(killTimeout);
+    console.log('removed');
+}
+
 server.on('connection', (ws) => {
     console.log('connected');
+    const currentUser = {};
 
     ws.on('close', () => {
         console.log('closed');
+        killTimeout = setTimeout(removeUser, 10000, {currentUser});
     })
 
     ws.on('message', message => {
         const data = JSON.parse(message);
         if(data.message === 'lm318') {
             if(noSuchUser(data.uid, data.ID)) {
+                currentUser.uid = data.uid;
+                currentUser.ID = data.ID;
                 const newUser ={
                     ws,
                     chatID:data.ID,
