6a1f0be69ab7c43a53923947c23617f5922d9340
ukrwebprom <ukrwebprom@gmail.com>
2023-04-20 17:08:53 +0300

    ndfgnfgn

--- index.js
+++ index.js
@@ -55,19 +55,14 @@ server.on('connection', (ws, req) => {
     const photo = url.searchParams.get('photo');
     const name = url.searchParams.get('name');
     console.log('connected', chatID, userID  );
-    if(noSuchUser(userID)) {
-        const newUser ={
+    const newUser ={
             ws,
             chatID,
             userID,
             photo,
             name,
         }
-        users.push(newUser);
-        
-    } else {
-        users.find(u => u.userID === userID).ws = ws;
-    }
+    users.push(newUser);
     const userList = getChatUsers(chatID);
     sendToAll(chatID, {message:'lm319', users:userList});
 
