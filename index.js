67bafc4974b31846a0deaf4e43dc389c8c1acc7f
ukrwebprom <ukrwebprom@gmail.com>
2023-04-28 01:27:22 +0300

    app

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const WebSocket = require('ws')
 const port = process.env.PORT || 8080
 const app = express()
 
-const server = new WebSocket({
-    'server': app
-})
+const server = new WebSocket({app});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
