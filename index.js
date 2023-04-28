3e3349a84740734de1863135b13636e828537375
ukrwebprom <ukrwebprom@gmail.com>
2023-04-28 15:58:26 +0300

    req.query

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const httpServer = http.createServer(app);
 const server = new WebSocketServer.WebSocketServer({server: httpServer});
 
 app.get('/isexist', (req, res) => {
-    console.log('check chat by id:', req.params.id);
+    console.log('check chat by id:', req.query);
     res.send('Hello World!')
   })
 httpServer.listen(port, () => {console.log('listening')})
