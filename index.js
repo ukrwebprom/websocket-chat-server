6ce9a3fe458dd9e6a4d3066a5cbc19ab040cef04
ukrwebprom <ukrwebprom@gmail.com>
2023-04-29 23:44:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ app.put('/chat', (req, res) =>{
     const chat = chats.find(c => c.id === chatID);
     console.log('chat to enter:', chatID)
     if(chat) {
-        chats.users.push({hash: req.body.hash, uid:req.body.uid});
-        updateChat(chats.users);
+        chat.users.push({hash: req.body.hash, uid:req.body.uid});
+        updateChat(chat.users);
         res.send(chat.users);
     }
     else throw new Error('CHAT IS NOT EXIST');
