33b1356b10d0d4aae90a3b5fc9cfa17360db5232
ukrwebprom <ukrwebprom@gmail.com>
2023-05-02 08:50:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 var sr = require('simple-random');
 
 const users = [];
